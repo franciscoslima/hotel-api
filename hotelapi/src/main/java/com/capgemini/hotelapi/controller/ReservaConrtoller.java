0ce9b48c4c9f8f853ac4b46b0fe99c1e567f1cb4
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-09-29 18:29:22 -0300

    criada classe service

--- hotelapi/src/main/java/com/capgemini/hotelapi/controller/ReservaConrtoller.java
+++ hotelapi/src/main/java/com/capgemini/hotelapi/controller/ReservaConrtoller.java
@@ -12,28 +12,28 @@ public class ReservaConrtoller {
 
     private final ReservaService reservaService;
 
-    public ReservaController(ReservaService reservaService) {
+    public ReservaConrtoller(ReservaService reservaService) {
         this.reservaService = reservaService;
     }
 
-    @GetMapping
+    @GetMapping("/listarreservas")
     public List<Reserva> listarTodas() {
         return reservaService.listarTodas();
     }
 
-    // seia interessante gerar um codigo da reserva para buscar por ele ?
-    @GetMapping("/{id}")
+    // seria interessante gerar um codigo da reserva para buscar por ele ?
+    @GetMapping("/buscarporreserva/{id}")
     public Reserva buscarPorId(@PathVariable Long id) {
         return reservaService.buscarPorId(id)
                 .orElseThrow(() -> new RuntimeException("Reserva não encontrada"));
     }
 
-    @PostMapping
-    public Reserva criar(@RequestBody Reserva reserva) {
+    @PostMapping("/fazerreserva")
+    public Reserva fazerReserva(@RequestBody Reserva reserva) {
         return reservaService.criarReserva(reserva);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/refazerreserva/{id}")
     public Reserva atualizar(@PathVariable Long id, @RequestBody Reserva reserva) {
         return reservaService.atualizarReserva(id, reserva);
     }
--- hotelapi/src/main/java/com/capgemini/hotelapi/service/ReservaService.java
+++ hotelapi/src/main/java/com/capgemini/hotelapi/service/ReservaService.java
@@ -1,4 +1,63 @@
 package com.capgemini.hotelapi.service;
 
+import com.capgemini.hotelapi.model.Reserva;
+import com.capgemini.hotelapi.repository.ReservaRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ReservaService {
+    private final ReservaRepository reservaRepository;
+
+    public ReservaService(ReservaRepository reservaRepository) {
+        this.reservaRepository = reservaRepository;
+    }
+
+    public List<Reserva> listarTodas() {
+        return reservaRepository.findAll();
+    }
+
+    public Optional<Reserva> buscarPorId(Long id) {
+        return reservaRepository.findById(id);
+    }
+
+    public Reserva criarReserva(Reserva reserva) {
+        // Regra de requisição: onde não pode check-out antes do check-in
+        if (reserva.getCheckOut().isBefore(reserva.getCheckIn())) {
+            throw new IllegalArgumentException("Primeiro você escolhe o check-in, depois o check-out");
+        }
+
+        // Regra de requisição: calcular valor total automaticamente (exemplo: 200 por dia)
+        long dias = ChronoUnit.DAYS.between(reserva.getCheckIn(), reserva.getCheckOut());
+        double valor = dias * 200.0;
+        reserva.setValorTotal(valor);
+
+        return reservaRepository.save(reserva);
+    }
+
+    // Atualizar reserva
+    public Reserva atualizarReserva(Long id, Reserva reservaAtualizada) {
+        return reservaRepository.findById(id)
+                .map(reserva -> {
+                    reserva.setCheckIn(reservaAtualizada.getCheckIn());
+                    reserva.setCheckOut(reservaAtualizada.getCheckOut());
+//                    reserva.setQuarto(reservaAtualizada.getQuarto());
+//                    reserva.setHospede(reservaAtualizada.getHospede());
+//                    reserva.setPropriedade(reservaAtualizada.getPropriedade());
+
+                    // para recalcular o valor
+                    long dias = ChronoUnit.DAYS.between(reserva.getCheckIn(), reserva.getCheckOut());
+                    reserva.setValorTotal(dias * 200.0);
+
+                    return reservaRepository.save(reserva);
+                }).orElseThrow(() -> new RuntimeException("Reserva não encontrada"));
+    }
+
+    // Deletar reserva
+    public void deletar(Long id) {
+        reservaRepository.deleteById(id);
+    }
 }
