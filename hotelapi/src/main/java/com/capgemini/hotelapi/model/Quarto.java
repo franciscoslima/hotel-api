b8c072570809cc2a50e0fa9335f4d13e35bc858b
jucarvalho <jucarvalho@outlook.com.br>
2025-09-30 16:52:36 -0300

    ajuste quartos

--- hotelapi/src/main/java/com/capgemini/hotelapi/model/Quarto.java
+++ hotelapi/src/main/java/com/capgemini/hotelapi/model/Quarto.java
@@ -1,19 +1,15 @@
 package com.capgemini.hotelapi.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Quarto {
 
     @Id
@@ -27,6 +23,7 @@ public class Quarto {
     private String descricao;
 
     @Enumerated(EnumType.STRING)
+    @Builder.Default
     private QuartoStatusEnum status = QuartoStatusEnum.DISPONIVEL;
 
     private Propriedade propriedade;
